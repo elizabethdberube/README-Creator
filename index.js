909105b2784630110e8c712257bc3c8dc4fbfc86
Lizzy <elizabethdberube@gmail.com>
2022-02-16 15:21:41 -0500

    Added function for writting README. Needs more work.

--- index.js
+++ index.js
@@ -1,52 +1,57 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-// TODO: Create an array of questions for user input
-inquirer.prompt([
-
-    {
-        type: 'input',
-        name: 'title',
-        message: 'Lets work on the description! What is the title of your README?',
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: ' What was your motivation for this project?',
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Why did you build this project?'
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'What problem does it solve?'
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'What did you learn from this project?'
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'Now lets about the installation! What are the steps required to install your project?'
-    },
-])
-    .then(function (answers) { console.log(answers) })
-
-//((answers) => {
 
 
-//});
+const askUser = () => {
+    return inquirer.prompt([
+
+        {
+            type: 'input',
+            message: 'Lets work on the description! What is the title of your README?',
+            name: 'title',
+        },
+        {
+            type: 'input',
+            message: ' What was your motivation for this project?',
+            name: 'description',
+        },
+        {
+            type: 'input',
+            message: 'Why did you build this project?',
+            name: 'description1',
+        },
+        {
+            type: 'input',
+            message: 'What problem does it solve?',
+            name: 'description2',
+        },
+        {
+            type: 'input',
+            message: 'What did you learn from this project?',
+            name: 'description3',
+        },
+        {
+            type: 'input',
+            message: 'Now lets about the installation! What are the steps required to install your project?',
+            name: 'installation',
+        },
+    ]);
+};
+const writeREADME = ({ title, description, description1, description2, description3, installation }) =>
+    `#${title}
+#Table of content
+${description}
+${description1}
+${description2}
+${description3}
+${installation}
+`;
+
+const init = () => {
+    askUser()
+        .then((answers) => fs.writeFileSync('README.md', writeREADME(answers)))
+
+};
 
 
-// TODO: Create a function to write README file
-//function writeToFile(fileName, data) { }
-
-// TODO: Create a function to initialize app
-//function init() { }
-
-// Function call to initialize app
-//init();
+init();
