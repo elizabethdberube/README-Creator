cc934fb28e5a61c5f8aae9183987f7c06992b600
Lizzy <elizabethdberube@gmail.com>
2022-02-19 19:55:11 -0500

    Finally got the anchor links working.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const gitURL;
-const gitUser = username;
+
 
 const askUser = () => {
     return inquirer.prompt([
@@ -9,12 +8,12 @@ const askUser = () => {
         {
             type: 'input',
             message: 'What is the title of your README?',
-            name: 'title',
+            name: 'title0',
         },
         {
             type: 'input',
             message: ' What was your motivation for this project?',
-            name: 'description',
+            name: 'description0',
         },
         {
             type: 'input',
@@ -34,111 +33,116 @@ const askUser = () => {
         {
             type: 'input',
             message: 'Now lets talk about the installation! What command would I need to install the necessary dependencies?',
-            name: 'installation',
+            name: 'installation0',
         },
         {
             type: 'input',
             message: 'How can this project be used?',
-            name: 'usage',
+            name: 'usage0',
         },
         {
             type: 'list',
             message: 'What kind of license should your project have?',
             choices: ['MIT', 'Apache License 2.0', 'GNU General Public License v3.0', 'BSD 2-Clause "Simplified" License', 'BSD 3-Clause "New" or "Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'Mozilla Public License 2.0', 'The Unlicense', 'There is no license for this project'],
-            name: 'license',
+            name: 'license0',
         },
         {
             type: 'input',
             message: 'How can a user contribute to this project?',
-            name: 'contribute',
+            name: 'contribute0',
         },
         {
             type: 'input',
             message: 'What command can be run to test this project?',
-            name: 'test',
+            name: 'test0',
         },
 
         {
             type: 'input',
             message: 'What email address can you be contacted at?',
-            name: 'email',
+            name: 'email0',
         },
 
         {
             type: 'input',
             message: 'What is your GitHub user name?',
-            name: 'username',
+            name: 'GitHub0',
         },
 
     ]);
 };
-const writeREADME = ({ title, description, description1, description2, description3, installation, usage, license, contribute, URL, test, email }) =>
-    `#${title}
-   
+const writeREADME = ({ title0, description0, description1, description2, description3, installation0, usage0, license0, contribute0, GitHub0, test0, email0 }) =>
+
+    `##${title0}
+
+ ---  
  
-##Description
+## Description
 A little bit about the motivation behind this project and why it was built. Also what it helped solve and what was learned from it.
-- ${description}
-- ${description1}
-- ${description2}
-- ${description3}
+1. ${description0}
+2. ${description1}
+3. ${description2}
+4. ${description3}
+
+---
 
 ##Table of content
 
-- [Description](#Description)
+* [Description](#description)
 
-- [Installation](#Installation)
+* [Installation](#installation)
 
-- [Usage](#Usage)
+* [Usage](#usage)
 
-- [License](#License)
+* [License](#license)
 
-- [Contributing](#Contributing)
+* [How to Contribute](#how-to-contribute)
 
-- [Tests](#tests)
+* [Tests](#tests)
 
-- [Questions](#Questions)
+* [Questions](#questions)
 
-##Installation -
+---
+
+# Installation -
 
 Use the following package manager
 
-${installation}
+${installation0}
 
 
-#Usage-
+## Usage-
 
 
-${usage}
+${usage0}
 
-#License-
+## License-
 
-This project is licensed by ${license}.
+This project is licensed by ${license0}.
 
    
-#How to contribute-
+## How to Contribute-
 
-${contribute}
+${contribute0}
 
-#Tests-
+## Tests-
 
 A user can run the following command to test this project 
 
-${test}
+${test0}
 
-#Questions-
+## Questions-
 
 If you have nay questions about this project then you can contact me directly at 
 
-${email}
+${email0}
 ----
 To see more of my work check out my GitHub-
 
-[]()
+ [GitHub](https://www.github.com/${GitHub0}) 
+`;
 
-link:  ${URL}
-`
-    ;
+
 
 const init = () => {
     askUser()
